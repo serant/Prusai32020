445014d30798e9abd1289d7753fab0b3d0d8e712
serant <seran.thirugnanam@gmail.com>
2016-08-15 23:46:13 -0400

    Adjusted offset to minimize need for auto leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -382,10 +382,10 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   #ifdef AUTO_BED_LEVELING_GRID
 
     // set the rectangle in which to probe
-    #define LEFT_PROBE_BED_POSITION 50
-    #define RIGHT_PROBE_BED_POSITION 150
-    #define BACK_PROBE_BED_POSITION 150
-    #define FRONT_PROBE_BED_POSITION 50
+    #define LEFT_PROBE_BED_POSITION 8
+    #define RIGHT_PROBE_BED_POSITION 164
+    #define BACK_PROBE_BED_POSITION 190
+    #define FRONT_PROBE_BED_POSITION 35
 
      // set the number of grid points per dimension
      // I wouldn't see a reason to go above 3 (=9 probing points on the bed)
@@ -410,7 +410,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 28
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -3
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -15.2
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -15.7
+  
 
   #define Z_RAISE_BEFORE_HOMING 25       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
@@ -764,7 +765,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // Use M206 command to correct for switch height offset to actual nozzle height. Store that setting with M500.
 //
 #define SERVO_ENDSTOPS {-1, -1, 0} // Servo index for X, Y, Z. Disable with -1
-#define SERVO_ENDSTOP_ANGLES {0,0, 0,0, 95,0} // X,Y,Z Axis Extend and Retract angles
+#define SERVO_ENDSTOP_ANGLES {0,0, 0,0, 91,0} // X,Y,Z Axis Extend and Retract angles
 
 /**********************************************************************\
  * Support for a filament diameter sensor
