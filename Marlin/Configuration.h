75529254f646676fcdc4bc982368c4cdfefa89dd
serant <seran.thirugnanam@gmail.com>
2017-05-27 12:45:31 -0400

    Revised settings for May 2017 move

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -410,7 +410,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 28
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -3
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -15.7
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -15.8
   
 
   #define Z_RAISE_BEFORE_HOMING 25       // (in mm) Raise Z before homing (G28) for Probe Clearance.
